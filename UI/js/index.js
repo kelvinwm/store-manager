b2509ffefe9edcada7c5b9ca485e6ea94d7b889a
Prince Kelvin <kelvinwmblogs@gmail.com>
2018-10-12 12:04:54 +0300

    [#161173920] Add admin dashboard, manage.html. Update javascript

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,4 +1,14 @@
 
 function homepage() {
-   window.location="home.html";
+
+	if (document.getElementById("category").value=='Attendant'){
+		window.location="home.html";
+	}
+	else if(document.getElementById("category").value=='Admin'){
+		window.location="manage.html";
+	}
+	else if(document.getElementById("category").value=='choose'){
+		alert("Please choose a role");
+
+	}
 }
